95db63cd495c6645a9bcbd0d0f481eb94c839fd1
tommy457 <boukhalfaml1011@gmail.com>
2023-11-21 03:54:18 -0800

    bug check

--- models/user.py
+++ models/user.py
@@ -12,5 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship(
-            "Place", backref="user", cascade="all, delete, delete-orphan")
