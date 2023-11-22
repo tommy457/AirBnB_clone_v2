d444537058790000b7a612966797cdde15a0333f
tommy457 <boukhalfaml1011@gmail.com>
2023-11-22 07:13:08 -0800

    add review model to query list and fix file mix-up

--- models/review.py
+++ models/review.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+""" Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey, Integer, Float
-from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import String, Column, ForeignKey
 
 
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship(
-                "Review", backref="place", cascade="all, delete")
-
-    else:
-        @property
-        def reviews(self):
-            from models import storage
-            from models.review import Review
-            objs = []
-            reviews = storage.all(Review)
-            for value in reviews:
-                if self.id == value.place_id:
-                    objs.append(value)
-            return objs
