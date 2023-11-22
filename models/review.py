22b4d3d049a21fa23dde1033d6f787a74bade836
tommy457 <boukhalfaml1011@gmail.com>
2023-11-22 07:22:31 -0800

    bug fix

--- models/review.py
+++ models/review.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 
 
-
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
