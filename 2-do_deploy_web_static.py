308bb1d0eaa3789a767c6dc51431bb634b70aa35
tommy457 <boukhalfaml1011@gmail.com>
2023-12-07 10:57:25 -0800

    exception handling and pycodesytle fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,39 +14,28 @@ def do_deploy(archive_path):
 
     file_name_with_ext = archive_path.split("/")[-1]
     file_name = file_name_with_ext.split(".")[0]
+    try:
+        put(archive_path, "/tmp/")
 
-    task_1 = put(archive_path, "/tmp/")
-    if task_1.failed:
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            file_name_with_ext, file_name))
+
+        run("rm -rf /tmp/{}".format(file_name_with_ext))
+
+        run('mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/'.format(file_name, file_name))
+
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            file_name))
+
+        run("rm -rf /data/web_static/current")
+
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".
+            format(file_name))
+
+        return True
+
+    except Exception:
         return False
-
-    task_2 = run("mkdir -p /data/web_static/releases/{}".format(file_name))
-    if task_2.failed:
-        return False
-
-    task_3 = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
-                 format(file_name_with_ext, file_name))
-    if task_3.failed:
-        return False
-
-    task_4 = run("rm -rf /tmp/{}".format(file_name_with_ext))
-    if task_4.failed:
-        return False
-
-    task7 = run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(file_name, file_name))
-    if task7.failed:
-        return False
-
-    result = run("rm -rf /data/web_static/releases/{}/web_static".format(
-        file_name))
-
-    task_5 = run("rm -rf /data/web_static/current")
-    if task_5.failed:
-        return False
-
-    task_6 = run("ln -s /data/web_static/releases/{} /data/web_static/current"
-                 .format(file_name))
-    if task_6.failed:
-        return False
-
-    return True
