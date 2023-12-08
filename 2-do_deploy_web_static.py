b65c86300795b84c124cb802e1186fe716c3b0a3
tommy457 <boukhalfaml1011@gmail.com>
2023-12-08 00:43:48 -0800

    yet another bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,8 +33,8 @@ def do_deploy(archive_path):
 
         run("rm -rf /data/web_static/current")
 
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(file_name))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
+                file_name))
 
         return True
 
