c4f8ab68f255a4281d913d855739550974bfe7af
tommy457 <boukhalfaml1011@gmail.com>
2023-12-08 00:31:28 -0800

    backslash

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,9 +18,9 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp/")
 
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        run("mkdir -p /data/web_static/releases/{}/".format(file_name))
 
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             file_name_with_ext, file_name))
 
         run("rm /tmp/{}".format(file_name_with_ext))
@@ -33,7 +33,7 @@ def do_deploy(archive_path):
 
         run("rm -rf /data/web_static/current")
 
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(file_name))
 
         return True
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -30,6 +30,7 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
+
     try:
         if not exists(archive_path):
             return False
@@ -39,12 +40,12 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp/")
 
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        run("mkdir -p /data/web_static/releases/{}/".format(file_name))
 
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             file_name_with_ext, file_name))
 
-        run("rm -rf /tmp/{}".format(file_name_with_ext))
+        run("rm /tmp/{}".format(file_name_with_ext))
 
         run('mv /data/web_static/releases/{}/web_static/* \
                 /data/web_static/releases/{}/'.format(file_name, file_name))
@@ -54,7 +55,7 @@ def do_deploy(archive_path):
 
         run("rm -rf /data/web_static/current")
 
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(file_name))
 
         return True
