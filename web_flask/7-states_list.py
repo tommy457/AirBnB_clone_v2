ecb5798d8b918de4ab23d8287f2d7d530745729e
tommy457 <boukhalfaml1011@gmail.com>
2023-12-25 12:33:04 -0800

    task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Modeul that starts a Flask web application"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Entry point runs the server"""
+    app.run(host="0.0.0.0")
