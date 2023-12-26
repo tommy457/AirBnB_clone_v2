949aef07b4e9748e7d0ab14ab608baa0a875d5de
tommy457 <boukhalfaml1011@gmail.com>
2023-12-26 01:27:46 -0800

    added docs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,12 +9,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Renders a template with list of states"""
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(error):
+    """Closes the current session"""
     storage.close()
 
 
