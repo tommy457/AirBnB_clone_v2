f9a9a51b9ad1f9df3b0a187a1372e6868dd6538f
tommy457 <boukhalfaml1011@gmail.com>
2023-12-26 01:59:37 -0800

    task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Modeul that starts a Flask web application"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Entry point runs the server"""
+    app.run(host="0.0.0.0")
