b496b1420f1497ac5e3f27d0c7bde2a3f09f2539
tommy457 <boukhalfaml1011@gmail.com>
2023-12-29 12:04:58 -0800

    task 100

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Modeul that starts a Flask web application"""
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_filters():
+    """Display a list of states, cities, places and amenities"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+
+    return render_template("100-hbnb.html",
+                           flag=True,
+                           states=states,
+                           places=places,
+                           amenities=amenities
+                           )
+
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Entry point runs the server"""
+    app.run(host="0.0.0.0", debug=True)
